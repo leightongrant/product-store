fe2566a67a872a2bc8cb72f2231d33fe438d9acb
Leighton Grant <oneal.grant@outlook.com>
2024-10-13 22:28:46 +0100

    changes static route

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -7,9 +7,6 @@ import {
 } from './controllers.js'
 const router = express.Router()
 
-router.get('/*', (req, res, next) => {
-	res.send(200)
-})
 router.get('/api/products', getProducts)
 router.post('/api/products', addProduct)
 router.delete('/api/products/:id', deleteProduct)
--- backend/server.js
+++ backend/server.js
@@ -4,21 +4,27 @@ import { connectDB } from './config/db.js'
 import router from './routes/routes.js'
 import errorHandler from './error/errorHandler.js'
 import cors from 'cors'
-import { URL } from 'node:url'
-const __dirname = new URL('..', import.meta.url).pathname
+// import { URL } from 'node:url'
+// const __dirname = new URL('..', import.meta.url).pathname
+import path from 'node:path'
 
-console.log(process.env.NODE_ENV)
-
+const __dirname = path.resolve()
 const app = express()
 const port = process.env.PORT || 8000
 connectDB()
 app.use(express.json())
 // app.use(morgan('tiny'))
 app.use(cors())
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(`${__dirname}/frontend/dist/`))
-}
 
 app.use(router)
 app.use(errorHandler)
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+	app.get('*', (req, res, next) => {
+		res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+	})
+}
+
 app.listen(port, () => console.log(`Server listening on port ${port || 8000}`))
